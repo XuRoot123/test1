a7d46ece8a4f4fed603d119cf4ef9fcf2e879ddd
XuRoot <asd9638527_0621@qq.com>
2025-03-10 11:21:10 +0800

    1233321

---
+++ HelloWord.java
@@ -0,0 +1,13 @@
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        int a =1;
+        int b =1;
+        int c = a+b;
+        System.out.println(a+b);
+        System.out.println(c);
+        System.out.println("Hello World!");
+        System.out.println("Hello World!");
+    }
+}
